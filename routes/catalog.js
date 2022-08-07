dce670d3a0bbd52b19ae3a0d3b6e9199d08ce0f1
aryan-debug <arynarora232004@gmail.com>
2022-08-07 02:30:05 -0700

    added package.json

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 // Require controller modules.
-var book_controller = require('../controllers/bookController');
-var author_controller = require('../controllers/authorController');
-var genre_controller = require('../controllers/genreController');
-var book_instance_controller = require('../controllers/bookinstanceController');
+var book_controller = require('../../express_local_library/controllers/bookController');
+var author_controller = require('../../express_local_library/controllers/authorController');
+var genre_controller = require('../../express_local_library/controllers/genreController');
+var book_instance_controller = require('../../express_local_library/controllers/bookinstanceController');
 
 /// BOOK ROUTES ///
 
